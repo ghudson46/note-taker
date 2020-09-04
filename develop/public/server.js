55c39cb3ce2db92273d79a98d99890c4000823b7
Garrett Hudson <garretthudson87@yahoo.com>
2020-09-04 13:11:42 -0400

    set up basic structure of get, post, and delete routes

--- develop/public/server.js
+++ develop/public/server.js
@@ -1,3 +1,4 @@
+// requiring the packages needed to run server
 const express = require('express');
 const path = require('path');
 const app = express();
@@ -6,15 +7,26 @@ const PORT = 3060;
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 
+// Routes
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, "notes.html"));
+});
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, "notes.html"));
+app.get('/api/notes', (req, res) => {
+  // Should read the `db.json` file and return all saved notes as JSON.
 });
 
+app.post('/api/notes', (req, res) => {
+  // Should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client.
+});
+
+
+
+// set up port the app is listening on
 app.listen(PORT, () => {
   console.log('App is listening on local host: ' + PORT);
 });
