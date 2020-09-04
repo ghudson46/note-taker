ac1eecf69e144887b82a236ace1be0616ff27950
Garrett Hudson <garretthudson87@yahoo.com>
2020-09-04 14:02:34 -0400

    saving progress for the day

--- README.md
+++ README.md
@@ -6,11 +6,11 @@ Create an application that can be used to write, save, and delete notes. This ap
 
 * The application frontend has already been created, it's your job to build the backend and connect the two.
 
-* The following HTML routes should be created:
+<!-- * The following HTML routes should be created:
 
   * GET `/notes` - Should return the `notes.html` file.
 
-  * GET `*` - Should return the `index.html` file
+  * GET `*` - Should return the `index.html` file -->
 
 * The application should have a `db.json` file on the backend that will be used to store and retrieve notes using the `fs` module.
 
--- develop/public/server.js
+++ develop/public/server.js
@@ -1,11 +1,21 @@
 // requiring the packages needed to run server
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 const app = express();
 const PORT = 3060;
 
+
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
+
+// creates body for json data
+var notes = [
+  {
+  "title":"Test Title",
+  "text":"Test text"
+  }
+];
 
 // Routes
 app.get('/notes', (req, res) => {
@@ -22,6 +32,10 @@ app.get('/api/notes', (req, res) => {
 
 app.post('/api/notes', (req, res) => {
   // Should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client.
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+  // Should receive a query parameter containing the id of a note to delete. This means you'll need to find a way to give each note a unique `id` when it's saved. In order to delete a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.
 });
 
 
