20c060357c9e5d13ed1d2535fd80c385b2e85f50
Garrett Hudson <garretthudson87@yahoo.com>
2020-09-07 19:32:42 -0400

    having trouble rendering html

--- develop/db/server.js
+++ develop/db/server.js
@@ -1,65 +1,46 @@
-// requiring the packages needed to run server
-const express = require('express');
-const path = require('path');
-const fs = require('fs');
-const localJson = require('db.json');
-const { notDeepStrictEqual } = require('assert');
+// Dependencies
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const localJson = require("./db.json")
 
-
+// Setting Up Server
 const app = express();
-const PORT = 3060;
+const PORT = 4567;
 
-
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// static middleware
+// Static Middleware
 app.use(express.static("../public"));
 
-// creates body for json data
-var notes = [
-  {
-  "title":"Test Title",
-  "text":"Test text"
-  }
-];
-
 // HTML Routes
-app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, "notes.html"));
-});
+app.get("/notes", function(req, res) {
+  res.sendFile(path.join(__dirname, "../public/notes.html"));
+  });
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
-});
+app.get("/", function(req, res) {
+     res.sendFile(path.join(__dirname, "../public/index.html"));
+  });
 
 
 // API Routes
-
-app.get('/api/notes', (req, res) => {
+app.get("/api/notes", function(req, res) {
   res.json(localJson);
-  // Should read the `db.json` file and return all saved notes as JSON.
-});
+}); 
 
-app.post('/api/notes', (req, res) => {
-  notes.push(req.body);
-  res.json(true);
-  // Should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client.
-});
+const noteArr = [];
 
-fs.appendFile("../db.json", JSON.stringify(notes), (err) => {
-  if (err) {
-    console.log(err);
-  }
-});
+app.post("/api/notes", function(req, res) {
+    noteArr.push(req.body);
+    res.json(true);
+  });
 
-app.delete('/api/notes/:id', (req, res) => {
-  // Should receive a query parameter containing the id of a note to delete. This means you'll need to find a way to give each note a unique `id` when it's saved. In order to delete a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.
-});
+fs.appendFile("../db.json", JSON.stringify(noteArr), function(err){
+  if (err) console.log(err);
+})
 
-
-
-// set up port the app is listening on
-app.listen(PORT, () => {
-  console.log('App is listening on local host: ' + PORT);
-});
+// Listening
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
