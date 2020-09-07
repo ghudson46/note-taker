1efc50884ce28859f9766362e2213f75ada1105e
Garrett Hudson <garretthudson87@yahoo.com>
2020-09-07 19:27:12 -0400

    update

--- develop/db/server.js
+++ develop/db/server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-const localJson = require('./db.json');
+const localJson = require('db.json');
 const { notDeepStrictEqual } = require('assert');
 
 
